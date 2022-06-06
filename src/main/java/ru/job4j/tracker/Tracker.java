e355572708e36d24fa2cd979b824e31274545280
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-06-07 00:18:36 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +23,34 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[100];
+        int rslSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (name.equals(item.getName())) {
+                rsl[rslSize] = item;
+                rslSize++;
+             }
+        }
+        rsl = Arrays.copyOf(rsl, rslSize);
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[100];
+        int rslSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                rsl[rslSize] = item;
+                rslSize++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, rslSize);
         return rsl;
     }
 }
