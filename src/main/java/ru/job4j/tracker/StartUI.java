f88c7cd9066b9aee3ee32c421de0dcea0d285c99
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-06-08 00:46:54 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,16 +2,39 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
 
+    public void init(Tracker tracker, Scanner scanner) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Select:");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал - " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        Item testItem = new Item();
-        testItem.setName("Заявка 1");
-        LocalDateTime itemCreated = testItem.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String formattedItemCreated = itemCreated.format(formatter);
-        System.out.println("Дата создания объекта Item - " + formattedItemCreated);
-        System.out.println(testItem);
+        Tracker tracker = new Tracker();
+        Scanner scanner = new Scanner(System.in);
+        new StartUI().init(tracker, scanner);
     }
 }
