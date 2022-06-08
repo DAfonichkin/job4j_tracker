9b2d6443fe81cff9e06aa00b7c40ed00f0151f5f
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-06-08 23:20:15 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748 #310578]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -54,7 +54,7 @@ public class StartUI {
                 int id = Integer.parseInt(scanner.nextLine());
                 Item item = tracker.findById(id);
                 if (item != null) {
-                    System.out.print(item);
+                    System.out.println(item);
                 } else {
                     System.out.println("Заявка с введенным id: " + id + " не найдена.");
                 }
