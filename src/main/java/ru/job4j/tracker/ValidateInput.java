564437b49da9b2246cd070bb7b9f7fcc100488b4
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-06-13 23:54:45 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class ValidateInput extends ConsoleInput {
+
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = Integer.parseInt(askStr(question));
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
+            }
+        } while (invalid);
+        return value;
+    }
+}
