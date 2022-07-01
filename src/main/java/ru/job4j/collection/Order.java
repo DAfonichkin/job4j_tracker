d1abb18471e2d89a1672894428a5d428a011a6e5
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-07-02 01:19:02 +0300

    1. Паспорт и жители [#212656 #317981]

--- src/main/java/ru/job4j/collection/Order.java
+++ src/main/java/ru/job4j/collection/Order.java
@@ -14,32 +14,32 @@ public class Order {
         this.name = name;
     }
 
-
     public String getName() {
         return name;
     }
 
     @Override
     public String toString() {
-        return "Order{" +
-                "number='" + number + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+        return "Order{"
+                + "number='" + number + '\''
+                + ", name='" + name + '\'' + '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Order order = (Order) o;
-        return Objects.equals(number, order.number) &&
-                Objects.equals(name, order.name);
+        return Objects.equals(number, order.number)
+                && Objects.equals(name, order.name);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(number, name);
     }
-
-
 }
