1887774892bf270ad572de0421c7d228010d0a07
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-07-12 00:22:11 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            StringBuilder start = new StringBuilder();
+            for (String el : value.split("/")) {
+                if (start.isEmpty()) {
+                    tmp.add(el);
+                    start.append(el);
+                } else {
+                    tmp.add(start + "/" + el);
+                    start.append("/");
+                    start.append(el);
+                }
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComparator());
+    }
+}
