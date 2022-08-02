0f13537d523389eee216a1c8c8fe15064947b39d
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-08-03 01:29:05 +0300

    7. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,12 +1,11 @@
 package ru.job4j.bank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Основной класс по работе с банковским сервисом
+ *
  * @author Dmtri Afonichkin
  * @version 1.0
  */
@@ -18,6 +17,7 @@ public class BankService {
 
     /**
      * Метод добавляет пользователя в систему.
+     *
      * @param user - добавляемый пользователь.
      */
     public void addUser(User user) {
@@ -26,8 +26,9 @@ public class BankService {
 
     /**
      * Метод добавляет аккаунт пользователя в систему.
+     *
      * @param passport - данные паспорта, по которым осуществляется поиск пользователя.
-     * @param account - добавляемый счет пользователя
+     * @param account  - добавляемый счет пользователя
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -41,42 +42,43 @@ public class BankService {
 
     /**
      * Метод находит пользователя по паспортным данным.
+     *
      * @param passport - данные паспорта, по которым осуществляется поиск пользователя.
      * @return возвращает найденного пользователя или null, если он не найден
      */
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (passport.equals(user.getPassport())) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод находит счет пользователя по паспортным данным и реквизитам счета.
-     * @param passport - данные паспорта, по которым осуществляется поиск пользователя.
+     *
+     * @param passport  - данные паспорта, по которым осуществляется поиск пользователя.
      * @param requisite - реквизиты, по которым осуществляется поиск счета.
      * @return возвращает найденный счет пользователя или null, если он не найден
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> userAccounts = users.get(user);
-            for (Account userAccount : userAccounts) {
-                if (requisite.equals(userAccount.getRequisite())) {
-                    return userAccount;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(acc -> acc.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
 
     /**
      * Метод предназначен для перечисления денег с одного счёта на другой счёт:
-     * @param srcPassport - данные паспорта, по которым осуществляется поиск пользователя-отправителя денег.
-     * @param srcRequisite - реквизиты, по которым осуществляется поиск счета пользователя-отправителя.
-     * @param destPassport - данные паспорта, по которым осуществляется поиск пользователя-получателя денег.
+     *
+     * @param srcPassport   - данные паспорта, по которым осуществляется поиск пользователя-отправителя денег.
+     * @param srcRequisite  - реквизиты, по которым осуществляется поиск счета пользователя-отправителя.
+     * @param destPassport  - данные паспорта, по которым осуществляется поиск пользователя-получателя денег.
      * @param destRequisite - реквизиты, по которым осуществляется поиск счета пользователя-получателя.
      * @return Возвращает Истина в случае успешного перевода,
      * если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят),
